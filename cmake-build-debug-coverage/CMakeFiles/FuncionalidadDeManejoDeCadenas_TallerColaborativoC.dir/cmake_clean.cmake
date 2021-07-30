00722170d6ef6f47b6e8d87814831f57fdd03aad
DonovanPS <donovan.picon@uptc.edu.co>
2021-07-30 14:29:38 -0500

    Se agrega las 4 primeras opciones

---
+++ cmake-build-debug-coverage/CMakeFiles/FuncionalidadDeManejoDeCadenas_TallerColaborativoC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FuncionalidadDeManejoDeCadenas_TallerColaborativoC.dir/main.c.obj"
+  "FuncionalidadDeManejoDeCadenas_TallerColaborativoC.exe"
+  "FuncionalidadDeManejoDeCadenas_TallerColaborativoC.exe.manifest"
+  "FuncionalidadDeManejoDeCadenas_TallerColaborativoC.pdb"
+  "libFuncionalidadDeManejoDeCadenas_TallerColaborativoC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FuncionalidadDeManejoDeCadenas_TallerColaborativoC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,9 +1,140 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <process.h>
+
+void convertirNombrePropio(){
+    int tamano,aux=0;
+    char cadena[255],cadenaNomPropio[255];
+    printf("\n>>Convertir en nombre propio<<"
+           "\nDigite la cadena de texto...");
+    gets(cadena);
+    tamano= strlen(cadena);
+    for (int i = 0; i < tamano; ++i) {
+        if (aux==i){
+            cadenaNomPropio[i]= toupper(cadena[i]);
+        } else{
+            if (cadena[i]==' '){
+                aux=i+1;
+                cadenaNomPropio[i]= cadena[i];
+            } else{
+                cadenaNomPropio[i]=cadena[i];
+            }
+        }
+    }
+    printf("%s \n",cadenaNomPropio);
+    system( "pause" );
+}
+
+
+void numVecesPalabra(){
+    int tamano,contador=0,aux=0;
+    char cadena[255],cadenaAux[50],palabraComparar[50];
+    printf("\n>>N%cmero de veces que existe una palabra<<"
+           "\nDigite la cadena de texto...",163);
+    gets(cadena);
+
+    tamano= strlen(cadena);
+
+    cadena[tamano]=' ';
+    cadena[tamano+1]='\0';
+
+    printf("Digite la palabra a buscar...");
+    gets(palabraComparar);
+
+    for (int i = 0; cadena[i] != '\0'; ++i) {
+        if (cadena[i]==' '){
+            cadenaAux[aux]='\0';
+            aux=0;
+
+            if (strcmp(palabraComparar,cadenaAux)==0){
+                contador++;
+            }
+            memset(cadenaAux,0,50);
+        } else{
+            cadenaAux[aux]=cadena[i];
+            aux++;
+        }
+    }
+    printf("\nla palabra %s se repite %i ves%s \n",palabraComparar,contador,contador>1? "es":"");
+    system( "pause" );
+}
+
+void encriptar(){
+    char cadena[255],cadenaEncriptada[255];
+    int tamano;
+
+    printf("\n>>Encriptar<<"
+           "\nDigite la cadena de texto a encpriptar...");
+    gets(cadena);
+
+
+    tamano= strlen(cadena);
+    for (int i = 0; i < tamano ; ++i) {
+
+        if (cadena[i]==toupper(cadena[i])){
+
+            *(cadena + i) = tolower(*(cadena + i));
+
+        } else{
+            *(cadena + i) = toupper(*(cadena + i));
+        }
+
+        //descarta los digitos
+        if(isalpha(*(cadena + i))){
+            //verifica los casos especiales X-Y-Z
+            if((*(cadena + i) > 87 && *(cadena + i)<91) ||(*(cadena + i)>119)){
+                *(cadena+i) = *(cadena+i) - 23;
+            } else{
+                *(cadena + i) = *(cadena + i) + 3;
+            }
+
+
+                //*(cadena + i) = *(cadena + i) + 3;
+        }
+    }
+
+
+    printf("\nLa frase encriptada es: %s\n",cadena);
+    system( "pause" );
+
+}
+
+void desencriptar(){
+
+    char cadena[255];
+    int tamano;
+
+    printf("\n>>Desencriptar<<"
+           "\nDigite la cadena de texto a desencpriptar...");
+    gets(cadena);
+    tamano= strlen(cadena);
+
+
+    for (int i = 0; i < tamano ; ++i) {
+
+        if (cadena[i]==toupper(cadena[i])){
+            *(cadena + i) = tolower(*(cadena + i));
+        } else{
+            *(cadena + i) = toupper(*(cadena + i));
+        }
+
+        if(isalpha(*(cadena + i))){
+            if(*(cadena + i) < 68  ||(*(cadena + i) >96 && *(cadena + i)<100)){
+                *(cadena+i)= *(cadena+i) + 23;
+            }else{
+                *(cadena+i)= *(cadena+i) - 3;
+            }
+        }
+    }
+    printf("\nLa frase desencriptada es: %s \n",cadena);
+    system( "pause" );
+}
 
 int main() {
 
     int op=0;
-    char *mainmenu = "\n>>>MENU PRINCIPAL<<<\n"
+    char *mainmenu = "\n      >>>MENU PRINCIPAL<<<\n"
                      "1. Convertir en nombre propio\n"
                      "2. N%cmero de veces que existe una palabra \n"
                      "3. Encriptar \n"
@@ -23,15 +154,19 @@ int main() {
 
         switch (op) {
             case 1:
+                convertirNombrePropio();
                 break;
 
             case 2:
+                numVecesPalabra();
                 break;
 
             case 3:
+                encriptar();
                 break;
 
             case 4:
+                desencriptar();
                 break;
 
             case 5:
